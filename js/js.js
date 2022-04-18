6176fbb21dc2126d8a6b572ac33e87cb17f805d0
martinjaureguibehere <martinjaureguibehere96@gmail.com>
2022-04-18 20:04:01 -0300

    segundo commit

---
+++ js/js.js
@@ -0,0 +1,5 @@
+let addToCartButton = document.querySelectorAll ("addToCart");
+addToShoppingCartButton.forEach ((addToCartButton) => {
+     addToCartButton.addEventListener("click",addToCartClicked);
+});
+
